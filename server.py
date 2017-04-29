936edf4cbf8cee453411e681d7fefc094a0c2437
Ivana SUN <Ivana@sunyimengde-MacBook-Pro.local>
2017-04-29 22:14:32 +0800

    fdfoasfe

--- server.py
+++ server.py
@@ -27,9 +27,8 @@ def index():
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT','5009'))
+        PORT = int(environ.get('PORT','5009'))
     except ValueError:
         PORT = 5009
 #    app.run(HOST, PORT)
-    socketio.run(app, port =  PORT, host= HOST)
-
+    socketio.run(app, port =  PORT, host= '0.0.0.0')
