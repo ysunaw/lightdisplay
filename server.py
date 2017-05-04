e7add7031c61ab75de32861e54d255a0faeb749e
Ivana SUN <Ivana@printIvana-is-doing-COMP3711.local>
2017-05-04 11:37:56 +0800

    mode section added but HTML not finished yet

--- server.py
+++ server.py
@@ -6,39 +6,45 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-
 from flask_socketio import send, emit
 from os import environ
 from flask import Flask
-#haha
-
 #app.run(environ.get('PORT'))
 
 @socketio.on('message')
 def handle_message(message):
-    print(message, file=sys.stderr)
-    send(message)
+	print(message, file=sys.stderr)
+	send(message)
+@app.route('/api/mode')
+def api_mode():
+	print("request",file = sys.stderr)
+	print (request,file = sys.stderr)
+	mode = request.args.get('mode')
+	print (mode, file = sys.stderr)
 
-@app.route('/api')
-def api():
+	socketio.emit('api_mode',{'mode': mode})
+	return render_template("hotel.html")
+
+@app.route('/api/facility')
+def api_facility():
 	print("request",file = sys.stderr)
 	print (request,file = sys.stderr)
 	action = request.args.get('action')
 	facility = request.args.get('facility')
-	print (action , file = sys.stderr)
-	
-	socketio.emit('api',{'action': action, 'facility' : facility})
+	print (action, file = sys.stderr)
+
+	socketio.emit('api_facility',{'action': action, 'facility' : facility})
 	return render_template("hotel.html")
 
 @app.route('/')
 def index():
-    return render_template("hotel.html")
+	return render_template("hotel.html")
 # app.run(port="8080")
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('PORT','5009'))
-    except ValueError:
-        PORT = 5009
+	HOST = environ.get('SERVER_HOST', 'localhost')
+	try:
+		PORT = int(environ.get('PORT','5009'))
+	except ValueError:
+		PORT = 5009
 #    app.run(HOST, PORT)
-    socketio.run(app, port =  PORT, host= '0.0.0.0')
+	socketio.run(app, port =  PORT, host= '0.0.0.0')
